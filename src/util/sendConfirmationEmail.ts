ea34cad155d3be24d1b21e4f3404464c22c37621
Dima <v3il@ukr.net>
2020-01-05 12:30:21 +0200

    Using basic email template

--- src/util/sendConfirmationEmail.ts
+++ src/util/sendConfirmationEmail.ts
@@ -2,22 +2,29 @@ import { SentMessageInfo } from 'nodemailer';
 
 import config from '../config';
 import { translateText, createMailTransporter, getTemplateHTML } from './index';
+import { User } from '../models';
 
-export default async (email: string, userHash: string, locale: string): Promise<SentMessageInfo> => {
+export default async (user: User, locale: string): Promise<SentMessageInfo> => {
     const transporter = createMailTransporter();
 
-    const html = await getTemplateHTML('email_confirmation', {
-        userHash,
-        locale,
-        serverURL: config.SERVER_URL,
-        message: translateText('confirmationEmailMessage', locale),
-        buttonText: translateText('confirmationEmailButtonText', locale),
+    const html = await getTemplateHTML('basic_email_template', {
+        message: translateText('resetPasswordEmailMessage', locale),
+        linkText: translateText('resetPasswordEmailLinkText', locale),
+        href: `${config.SERVER_URL}/auth/verify_email/${user.userHash}`,
     });
+
+    // const html = await getTemplateHTML('email_confirmation', {
+    //     userHash,
+    //     locale,
+    //     serverURL: config.SERVER_URL,
+    //     message: translateText('confirmationEmailMessage', locale),
+    //     buttonText: translateText('confirmationEmailButtonText', locale),
+    // });
 
     const mailOptions = {
         html,
         from: `"Rest Finder" <${config.MAIL_USER}>`,
-        to: email,
+        to: user.email,
         subject: translateText('confirmationEmailTitle', locale),
     };
 
